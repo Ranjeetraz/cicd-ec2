615fdcb03a0674257413a32ed965974682b61e29
Ranjeetraz <ranjeetkumarmth982@gmail.com>
2025-02-12 16:39:04 +0530

    commit12

--- pro1/app/views.py
+++ pro1/app/views.py
@@ -5,7 +5,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['name'] = 'Ranjeet Gupta'  # Dynamic data template mein bhejna
+        context['name'] = 'Ranjeet Gupta kumar'  # Dynamic data template mein bhejna
         return context
 
 
