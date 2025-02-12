45ce92eeee2898bd20ab520e439c1d2456338ce9
Ranjeetraz <ranjeetkumarmth982@gmail.com>
2025-02-12 16:01:25 +0530

    commit10

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,34 +1,21 @@
 #!/usr/bin/env bash
 
-# Kill any running Django development servers in the background
+# Kill any running Django servers
 sudo pkill -f runserver
 
-# Kill frontend servers if you are deploying any frontend (uncomment if needed)
-# sudo pkill -f tailwind
-# sudo pkill -f node
-
-# Navigate to the project directory
-
-
-# # Create and activate a virtual environment
-# python3 -m venv venv
-# source venv/bin/activate
-# ======================
+# Update packages and install python-venv
 sudo apt update
-sudo apt install python3-venv
+sudo apt install -y python3-venv
+
+# Set up virtual environment
 python3 -m venv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 
-# pip install -r requirements.txt
+# Navigate to project directory
+cd /home/ubuntu/pro1
 
-
-# Install dependencies from requirements.txt
+# Install dependencies
 pip install -r /home/ubuntu/pro1/pro1/requirements.txt
 
-
-# # Run Django development server in a detached screen session (background)
-# screen -d -m python3 manage.py runserver 0.0.0.0:8000
-# ========================
-python3 /home/ubuntu/pro1/pro1/manage.py runserver 0.0.0.0:8000
-
-
+# Run Django server in the background
+nohup python3 /home/ubuntu/pro1/pro1/manage.py runserver 0.0.0.0:8000 &
